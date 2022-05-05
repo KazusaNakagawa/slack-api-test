76949fc2decaf24ecae287bfa548b97129a09af1
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-05-05 20:50:23 +0900

    feat: Specify channel & mention

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 import json
 import logging
 import os
+import pathlib
 import requests
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+LOG_DIR = './log'
+USER_ID = os.environ.get("USER_ID")
+
+"""
+ref: https://api.slack.com/apps/<APP ID>/incoming-webhooks?
+naming rule: SLACK_Webhook_URL_<channel name>
+"""
+# SLACK_Webhook_URL_RANDOM = os.environ.get("SLACK_Webhook_URL_RANDOM")
+# SLACK_Webhook_URL_SAMPLE_MSG_APP = os.environ.get("SLACK_Webhook_URL_SAMPLE_MSG_APP")
+SLACK_Webhook_URL_ERROR_TEST = os.environ.get("SLACK_Webhook_URL_ERROR_TEST")
+
+if not pathlib.Path(LOG_DIR).exists():
+    pathlib.Path(LOG_DIR).mkdir()
+
 logger = logging.getLogger(os.path.basename(__file__))
 logging.basicConfig(
     level='INFO',
-    filename='log/app.log',
+    filename=f"{LOG_DIR}/app.log",
     filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
 
 def message(text='test message'):
-    payload = {"text": f"{text}"}
-    url = os.environ.get("SLACK_SAMPLE_MSG_APP")
+    payload = {'text': f"{text}"}
+    url = SLACK_Webhook_URL_ERROR_TEST
 
     try:
-        if 'Error' in text:
-            raise Exception
+        if 'Alert' in text:
+            r = requests.post(url=url, data=json.dumps(payload))
+            logger.info({
+                'status': r.status_code,
+                'msg': payload,
+            })
         else:
             logger.info({'msg': 'Done'})
 
     except Exception as ex:
-        r = requests.post(url=url, data=json.dumps(payload))
-        logger.error({
-            'ex': ex,
-            'status': r.status_code,
-            'msg': payload,
-        })
+        logger.error({'ex': ex})
 
 
 if __name__ == '__main__':
-    message('⚠️ Error: test Alert')
+    message('⚠️ <!channel> Error: test Alert')
+    message('⚠️ <!here> Error: test Alert')
+    message(f"⚠️ :frowning: <@{USER_ID}> Error: test Alert")
     message()
